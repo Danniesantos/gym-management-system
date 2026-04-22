bd8d5e93e41ae5fc92494f47c84c126655faebdc
Danniesantos <danielardesantos@gmail.com>
2026-04-22 15:16:10 -0300

    criado planoRepository

---
+++ src/main/java/com/academia/gym/repository/PlanoRepository.java
@@ -0,0 +1,7 @@
+package com.academia.gym.repository;
+
+import com.academia.gym.model.plano.Plano;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanoRepository extends JpaRepository<Plano,Long> {
+}
