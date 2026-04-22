9f45d47cefc0199d504b94e4e866df4a2dab3688
Danniesantos <danielardesantos@gmail.com>
2026-04-22 15:26:39 -0300

    criado MatriculaRepository e metodos

--- src/main/java/com/academia/gym/repository/MatriculaRepository.java
+++ src/main/java/com/academia/gym/repository/MatriculaRepository.java
@@ -5,7 +5,15 @@ import com.academia.gym.model.matricula.StatusMatricula;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
+
+    List<Matricula> findByAlunoId(Long alunoId);
+
     boolean existsByAlunoIdAndStatus(Long alunoId, StatusMatricula status);
+
+    Optional<Matricula> findByAlunoIdAndStatus(Long alunoId, StatusMatricula status);
 }
