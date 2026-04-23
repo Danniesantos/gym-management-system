f718b7607835e892e6136c5e039e8bb9cb70ea9b
Danniesantos <danielardesantos@gmail.com>
2026-04-23 16:33:52 -0300

    refatoração de atributo

--- src/main/java/com/academia/gym/dto/plano/PlanoResponseDTO.java
+++ src/main/java/com/academia/gym/dto/plano/PlanoResponseDTO.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 
 @Builder
 public record PlanoResponseDTO(Long id,
-                               TipoDePlanoEnum tipo,
+                               TipoDePlanoEnum tipoPlano,
                                Integer duracaoDias,
                                BigDecimal preco) {
 
