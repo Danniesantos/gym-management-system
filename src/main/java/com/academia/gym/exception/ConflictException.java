820814bfa721b6d78db1ea043b63c6ab116749d5
Danniesantos <danielardesantos@gmail.com>
2026-04-28 17:50:01 -0300

    adiciona classes de exceções customizadas

---
+++ src/main/java/com/academia/gym/exception/ConflictException.java
@@ -0,0 +1,8 @@
+package com.academia.gym.exception;
+
+public class ConflictException extends RuntimeException {
+
+    public ConflictException(String mensagem) {
+        super(mensagem);
+    }
+}
