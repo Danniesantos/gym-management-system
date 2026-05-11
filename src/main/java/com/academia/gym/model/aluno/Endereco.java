cc8d1a9f52cc1584a7468c4c28c3f53bb420b305
Danniesantos <danielardesantos@gmail.com>
2026-05-11 19:13:44 -0300

    refactor: ajusta modelagem e validações da entidade endereco

--- src/main/java/com/academia/gym/model/aluno/Endereco.java
+++ src/main/java/com/academia/gym/model/aluno/Endereco.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Builder
 @Getter
 @Setter
 @AllArgsConstructor
@@ -16,11 +18,11 @@ public class Endereco {
 
     @Column(length = 8, nullable = false)
     private String cep;
-    @Column(length = 100,nullable = false)
+    @Column(length = 100, nullable = false)
     private String rua;
     @Column(length = 5)
     private String numero;
-    @Column(length = 50,nullable = false)
+    @Column(length = 50, nullable = false)
     private String bairro;
     @Column(length = 50, nullable = false)
     private String cidade;
